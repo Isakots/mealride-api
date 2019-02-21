81029d90244a1ecad95eece2d8547137d136e758
Marcell Csókási <csokasi.marcell@gmail.com>
2019-02-21 11:35:58 +0100

    Initial commit

---
+++ src/main/java/hu/student/projlab/mealride_api/MealrideApiApplication.java
@@ -0,0 +1,13 @@
+package hu.student.projlab.mealride_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealrideApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealrideApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/student/projlab/mealride_api/MealrideApiApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.student.projlab.mealride_api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MealrideApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
